c3b23ac5f8ac465386ee3870d0c1290e4fe5f28a
lakshmi srujana g <sruj.gl@gmail.com>
2026-01-13 09:55:59 +0530

    jsevents

--- JS Assignments/day11Assignment2.js
+++ JS Assignments/day11Assignment2.js
@@ -1,12 +1,53 @@
-const paymentSection = document.getElementById("paymentSection");
-const payBtn = document.getElementById("payBtn");
-
-// Click event on div
-paymentSection.addEventListener("click", () => {
-  console.log("Payment Section clicked");
+document.getElementById("paymentSection").addEventListener("click", function () {
+    console.log("Payment Section clicked");
 });
 
-// Click event on button
-payBtn.addEventListener("click", () => {
-  console.log("Pay Premium button clicked");
+
+document.getElementById("payBtn").addEventListener("click", function () {
+    console.log("Pay Premium button clicked");
+});
+
+
+document.getElementById("policyContainer").addEventListener(
+    "click",
+    function () {
+        console.log("Parent: Validating user before showing policy");
+    },
+    true 
+);
+
+
+document.getElementById("viewPolicyBtn").addEventListener(
+    "click",
+    function () {
+        console.log("Child: Showing policy details");
+    },
+    true 
+);
+
+
+document.querySelector(".policyCard").addEventListener("click", function () {
+    console.log("Navigating to policy details page");
+});
+
+
+document.querySelector(".deleteBtn").addEventListener("click", function (event) {
+    event.stopPropagation(); 
+    console.log("Policy deleted");
+});
+
+
+
+document.querySelectorAll(".claimRow").forEach(row => {
+    row.addEventListener("click", function () {
+        console.log("Opening Claim Details");
+    });
+});
+
+
+document.querySelectorAll(".approveBtn").forEach(button => {
+    button.addEventListener("click", function (event) {
+        event.stopPropagation(); 
+        console.log("Claim Approved");
+    });
 });
