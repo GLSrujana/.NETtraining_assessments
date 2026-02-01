1b8d9ac995595f3c9ae4b0b7a0a32ee879d96d61
lakshmi srujana g <sruj.gl@gmail.com>
2026-02-01 14:40:53 +0530

    week-5 assignment class library

---
+++ srujana-week5-AssignmentClassLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Weekly_Assignment_5CL
+{
+    public class Class1
+    {
+        public static double CalculateNetSalary(double basicSalary)
+        {
+            if (basicSalary <= 0)
+            {
+                throw new ArgumentException("Basic salary must be greater than zero");
+            }
+
+            double hra = 0.20 * basicSalary;
+            double da = 0.10 * basicSalary;
+            double pf = 0;
+
+            if (basicSalary >= 15000)
+            {
+                pf = 0.12 * basicSalary;
+            }
+
+            return basicSalary + hra + da - pf;
+        }
+
+    }
+}
